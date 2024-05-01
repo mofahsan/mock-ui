246c471252a2cfb33d0ac9269c013145b336a990
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2024-05-01 19:15:41 +0530

    updated env

--- src/env/env.js
+++ src/env/env.js
@@ -1,6 +1,6 @@
 // prod
 export const env = {
-  sandBox: "https://fis-buyer-staging.ondc.org/api",
+  sandBox: "https://fis-staging.ondc.org/api",
   mockServer: "https://fis-seller-staging.ondc.org",
 };
 
